7a4bc6aabb8eca668a2c617d7f2a324eaf8536f8
thomas@winterling-labs.com <thomas@winterling-labs.com>
2018-11-22 07:38:42 +0100

    Create Helper

---
+++ helper.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     mod_wl_audioplayer
+ * @author      Thomas Winterling
+ * @email       info@winterling-labs.com
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_dd-slider
+ *
+ * @since  Version 1.0.0.0
+ */
+class modWL_AudioplayerHelper
+{
+
+
+
+}
