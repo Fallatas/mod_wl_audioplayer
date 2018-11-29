592a05eac50aef97d9939f6071b8d26aeea1ca02
thomas@winterling-labs.com <thomas@winterling-labs.com>
2018-11-29 08:23:27 +0100

    Integrate audioplayer

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,3 +12,53 @@ JHtml::_('stylesheet', 'mod_wl_audioplayer/style.css', array('version' => 'auto'
 JHtml::_('script', 'mod_dd_slider/audiocontrols.js', array('version' => 'auto', 'relative' => true));
 
 ?>
+
+<audio id="song">
+    <source src="./mp3/test.mp3" type="audio/mp3" />
+</audio> <br>
+<div id="wl-audioplayer">
+    <div class="top">
+        <div id="defaultBar" class="defalutBar">
+            <div id="progressBar" class="progressBar">
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="image-box">
+        </div>
+        <div class="userinterface">
+            <div class="timeline">
+                <div class="timefield">
+                    <div class="border">
+                        <span id="currentTime">0:00</span>
+                        /<span id="fullDuration">0:00</span>
+                    </div>
+                </div>
+            </div>
+            <div class="buttons">
+                <div id="playButton" class="playbutton">
+                    <span>Play</span>
+                </div>
+                <div id="prev" class="prevbutton">
+                    <span>Prev</span>
+                </div>
+                <div id="next" class="nextbutton">
+                    <span>Next</span>
+                </div>
+                <div id="playlist" class="playlistbutton">
+                    <span>Playlist</span>
+                </div>
+                <div id="muteButton" class="mutebutton">
+                    <span>Mute</span>
+                </div>
+            </div>
+            <div id="title" class="titles">
+                <div id="titles">
+                    <div class="border">
+                        <span id="songTitle">Artist - Current Song</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
