bafd381a233959017b06ef9f5d76915f5aef3280
thomas@winterling-labs.com <thomas@winterling-labs.com>
2018-11-29 06:42:47 +0100

    Add stylesheet and audiocontrols

---
+++ tmpl/default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     mod_wl_audioplayer
+ * @author      Thomas Winterling
+ * @email       info@winterling-labs.com
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('stylesheet', 'mod_wl_audioplayer/style.css', array('version' => 'auto', 'relative' => true));
+JHtml::_('script', 'mod_dd_slider/audiocontrols.js', array('version' => 'auto', 'relative' => true));
+
+?>
